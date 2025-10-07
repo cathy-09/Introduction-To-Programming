2f4c8037eed11d1abf9a1d0297847904dafe651d
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-10-07 21:50:50 +0300

    Create Task1.cpp

---
+++ Week 1/Tasks/cppFiles/Task1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int januaryDays;
+    cin >> januaryDays;
+    if (januaryDays <= 31)
+    {
+        cout << "" << true << "";
+    }
+    else
+    {
+        cout << "" << false << "";
+    }
+}
