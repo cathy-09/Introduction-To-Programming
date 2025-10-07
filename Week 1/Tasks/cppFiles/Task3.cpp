c623c64bc238d8fb52a62e98b8c93e8adcf391a1
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-10-07 22:48:07 +0300

    New File Tasks

---
+++ Week 1/Tasks/cppFiles/Task3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const double mile = 0.621371;
+    double a;
+    cin >> a;
+    double kilometersToMiles = a * mile;
+    cout << "" << kilometersToMiles << "";
+}
