34629747b6ed5644af5e8506903485bf050edb87
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-10-10 20:09:58 +0300

    Update Task10.cpp

--- Week 1/Tasks/cppFiles/Task10.cpp
+++ Week 1/Tasks/cppFiles/Task10.cpp
@@ -12,3 +12,20 @@ int main()
     };
     cout << "" << reversed << "";
 }
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    short  a, thousands = 0, hundreds = 0, tens = 0, units = 0, reversed = 0;
+    cin >> a;
+	thousands = a / 1000;
+	hundreds = (a / 100) % 10;
+	tens = (a / 10) % 10;
+	units = a % 10;
+	reversed = units * 1000 + tens * 100 + hundreds * 10 + thousands;
+    cout << "" << reversed << "";
+}
